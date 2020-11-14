11a8ddc56c4184a039be139337aaf0301a96d80a
AcnoZed <Me.ouss.am@outlook.de>
2020-11-14 19:35:53 +0100

    Took out local repo init

--- Create.py
+++ Create.py
@@ -6,10 +6,9 @@ from github import Github
 
 def create_Repo():
     script_name = sys.argv[1]
-    repo_Git = Repo(script_name)
     token = input("Token :")
     github = Github(token)
     user = github.get_user()
-    repo_GitHub = user.create_repo(sys.argv)
+    repo_GitHub = user.create_repo(script_name)
 
 create_Repo()
